727956d2245e03e4007846f13332d566a92284bf
Jacob Sanford <jsanford@unb.ca>
2023-01-24 16:40:24 -0400

    Allow application to deploy locally

--- build/scripts/run.sh
+++ build/scripts/run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+if [ "$DEPLOY_ENV" = "local" ]; then
+  # Delete all references to SSL in the apache conf.
+  sed -i '/^SSL/d' /usr/local/apache2/conf/httpd.conf
+  # Listen on the default port.
+  sed -i "s|Listen 443|Listen 80|g" /usr/local/apache2/conf/httpd.conf
+fi
+
 /etc/init.d/shibd start
 /usr/local/bin/httpd-foreground
